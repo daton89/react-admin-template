4a3fc5cfa9452a61799f1cb761bdfce106fa8089
Toni D'Angelo <tonydangelo123@gmail.com>
2018-07-11 16:10:31 +0200

    add password with validation

--- src/validation/specific.js
+++ src/validation/specific.js
@@ -1,3 +1,10 @@
-export const required = value => value ? undefined : 'Value is required'
-export const minLength = value => value.length < 4 ? 'Value must be at least 4 characters' : undefined
-export const maxLength = value => value.length > 10 ? 'Value is too long' : undefined
+export const required = value => (value ? undefined : "Value is required");
+
+export const minLength = value =>
+  value.length < 4 ? "Value must be at least 4 characters" : undefined;
+
+export const maxLength = value =>
+  value.length > 10 ? "Value is too long" : undefined;
+
+export const matchesPassword = (value, allValues) =>
+  value !== allValues.password ? "Passwords must match" : undefined;
